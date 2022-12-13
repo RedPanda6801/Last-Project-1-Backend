65b7b86c553e02d4744a9af997221cf11644b49d
Redpanda6801 <redpanda6801@gmail.com>
2022-12-13 16:13:54 +0900

    [Ver 1.2.2] Cycle 데이터 전체 조회 및 날짜별 조회

--- volume/backend/routes/device.js
+++ volume/backend/routes/device.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const { verifyToken } = require("../lib/tokenUtil");
-const { addDevice } = require("../controllers/device");
+const {
+  addDevice,
+  findAllCycleData,
+  findTodayCycleData,
+} = require("../controllers/device");
 const router = express.Router();
 
 router.get("/insert", addDevice);
-
+router.get("/find-cycle-all", verifyToken, findAllCycleData);
+router.get("/find-cycle-today", findTodayCycleData);
 module.exports = router;
