0f57fc5600d5b085b5fded95a8f5f636ebc6db8f
Redpanda6801 <redpanda6801@gmail.com>
2022-12-16 10:13:01 +0900

    [Ver 1.5.0] 다이스 값 추가 함수 구현(검증X)

--- volume/backend/dao/logDao.js
+++ volume/backend/dao/logDao.js
@@ -1,11 +1,21 @@
 const logger = require("../lib/logger");
+const { Dice } = require("../models");
 
 // 한 사이클에서 받은 다이스 숫자 값을 담은 배열을 받음
-exports.setDiceNum = async (numArr) => {
+exports.setDiceNum = async (arr) => {
   try {
-    console.log(numArr);
+    console.log("들어온 배열:", arr);
+    const result = await Dice.create({
+      one: arr[0],
+      two: arr[1],
+      three: arr[2],
+      four: arr[3],
+      five: arr[4],
+      six: arr[5],
+    });
+    return result;
   } catch (error) {
     logger.error(error.toString());
-    return res.status;
+    return new Error(error);
   }
 };
